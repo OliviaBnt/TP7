03e8dfd49307e72b1317f724447fd626289c117f
Chambon Lea <lchambon@bs406-s16-06.insa-lyon.fr>
2018-04-22 23:36:21 +0200

    Constructeur de copie

--- string.cpp
+++ string.cpp
@@ -26,6 +26,16 @@ void String::printtest(){
 }
 
 /*
+ * * Constructeur de copie*/
+String::String(const String &str) : tableau_(0), capacity_(str.capacity_), size_(str.size_)
+{
+    tableau_ = new char[capacity_+1];
+    for(int i(0); i < size_+1; i++){
+        tableau_[i] = str.tableau_[i];
+    }
+}
+
+/*
 * Methode de debuggage
 *
 void String::debug(void){
